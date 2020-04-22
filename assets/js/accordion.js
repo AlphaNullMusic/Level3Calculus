0c7909673cbd06a16704360c1de38b263faf7b68
Roman Avery <roman.avery@whitbycollegiate.school.nz>
2020-04-22 00:30:37 +0000

    Added icons, changed styling on all content pages and finished accordion on topics page.

---
+++ assets/js/accordion.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  var accordion = $("#accordion");
+  var i;
+
+  for (i = 0; i < accordion.children('h3').length; i++) {
+    accordion.children('h3')[i].addEventListener("click", function() {
+      $(this).siblings('h3').removeClass('active').next().removeAttr('style');
+      this.classList.toggle("active");
+
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+}
