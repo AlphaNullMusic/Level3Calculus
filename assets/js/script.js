cbbe1187c96e431b8efbac84e587b58209ab0e78
Roman Avery <roman.avery@whitbycollegiate.school.nz>
2020-05-04 02:50:50 +0000

    Added links to icons on homepage. Merged accordion.js to script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,6 +58,25 @@ function toggleNavbar() {
   }
 }
 
+function initAccordion() {
+  var accordion = $("#accordion");
+  var i;
+
+  for (i = 0; i < accordion.children('h3').length; i++) {
+    accordion.children('h3')[i].addEventListener("click", function() {
+      $(this).siblings('h3').removeClass('active').next().removeAttr('style');
+      this.classList.toggle("active");
+
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+}
+
 window.onload = function() {
   checkCookie();
   document.getElementById('navbar_toggle').addEventListener("click", toggleNavbar);
